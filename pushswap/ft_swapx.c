53ac89054ccf84b7c6869ee7fceaad4e3753fd1f
Luca | lsordo <sordo.luca@gmail.com>
2022-12-13 05:56:57 +0100

    First commit 	new file:   ft_cmp.c 	new file:   ft_swa.c 	new file:   ft_swapx.c

---
+++ pushswap/ft_swapx.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swapx.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 05:23:44 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/13 05:35:40 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void ft_swapx(t_stk *lst1, t_stk *lst2)
+{
+	int	tnum;
+	int	toix;
+	int	tsix;
+
+	tnum = lst1->num;
+	toix = lst1->oix;
+	tsix = lst1->six;
+	lst1->num = lst2->num;
+	lst1->oix = lst2->oix;
+	lst1->six = lst2->six;
+	lst2->num = tnum;
+	lst2->oix = toix;
+	lst2->six = tsix;
+}
