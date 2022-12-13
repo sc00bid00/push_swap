6654de093d6d55ea39826e1bd0dc6bace48bdb60
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-13 18:31:55 +0100

    Modify 	modified:   Makefile 	modified:   main.c 	modified:   push_swap.h

--- main.c
+++ main.c
@@ -13,18 +13,24 @@ int main(int argc, char **argv)
 		stka = ft_newstk();
 		ft_addstk(&stka, &argv[1], argc - 1);
 		ft_idxstk(&stka,ft_cmp);
+		ft_printf("\nStack a & b\n");
 		ft_prtstk(stka, stkb);
-		ft_printf("\n");
 		ft_px(&stka, &stkb);
 		ft_px(&stka, &stkb);
 		ft_px(&stka, &stkb);
 		ft_px(&stka, &stkb);
+		ft_printf("\n4x pa\n");
 		ft_prtstk(stka, stkb);
 		ft_px(&stkb, &stka);
 		ft_px(&stkb, &stka);
+		ft_printf("\n2x pb\n");
 		ft_prtstk(stka, stkb);
 		ft_sx(&stka);
 		ft_sx(&stkb);
+		ft_printf("\nss (sa sb)\n");
+		ft_prtstk(stka, stkb);
+		ft_rx(&stka);
+		ft_printf("\nra\n");
 		ft_prtstk(stka, stkb);
 	}
 	return (0);
