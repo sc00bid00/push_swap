f6c1cf71b391392ec0839b13b959b9221f81ad8f
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-13 17:46:56 +0100

    First commit 	new file:   ft_px.c 	new file:   ft_sx.c

---
+++ ft_px.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_px.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 12:21:06 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/13 17:33:40 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+// head of stack a becomes head of stack b
+
+void	ft_px(t_stk **src, t_stk **dst)
+{
+	t_stk	*tmp;
+
+	if (*src == NULL)
+		;
+	else
+	{
+		if(*dst == NULL)
+			*dst = ft_newstk();
+		tmp = *src;
+		*src = (*src)->next;
+		tmp->next = *dst;
+		*dst = tmp;
+		if((*dst)->next->oix == 0)
+			(*dst)->next = NULL;
+	}
+}
