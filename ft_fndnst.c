c9c8b21c01ff6568c06f765cb84032a5d1f5dc2f
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-14 11:03:57 +0100

    First commit 	new file:   ft_fndnst.c 	new file:   ft_pstkid.c

---
+++ ft_fndnst.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fndnst.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 09:43:48 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/14 10:43:50 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+// finds the nearest element either head or tail
+
+int	ft_fndnst(t_stk *stk, int x)
+{
+	int	n;
+	int	p;
+
+	n = ft_stksize(stk);
+	p = 1;
+	while (p <= n && stk->six != x)
+	{
+		p++;
+		stk = stk->next;
+	}
+	return (p);
+}
+
---
+++ ft_pstkid.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pstkid.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 10:14:29 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/14 10:21:08 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_pstkid(t_stk *stk)
+{
+	ft_printf("oix\tsix\tnum\n");
+	ft_printf("-------------------\n");
+	while (stk)
+	{
+		ft_printf("%d\t%d\t%d\n", stk->oix, stk->six, stk->num);
+		stk = stk->next;
+	}
+
+}
