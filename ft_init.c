49ca9d080560b0c75d2f71b9465377b52feb214e
Luca | lsordo <sordo.luca@gmail.com>
2022-12-18 21:45:41 +0100

    Modify

--- ft_init.c
+++ ft_init.c
@@ -6,13 +6,43 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 17:03:17 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/18 18:08:32 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/18 21:43:43 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_isdble(t_stk *arg)
+void	ft_abort(char **arr, t_stk **arg)
+{
+	int		j;
+	t_stk	*tmp;
+
+	j = 0;
+	if (arr[0])
+	{
+		while (arr[j])
+		{
+			free(arr[j]);
+			j++;
+		}
+		free(arr);
+
+	}
+	if (*arg)
+	{
+		while (arg && *arg)
+		{
+			if ((*arg)->next)
+				tmp = (*arg)->next;
+			free(*arg);
+			*arg = tmp;
+		}
+		ft_printf("Error\n");
+		exit(0);
+	}
+}
+
+int		ft_isdble(t_stk *arg)
 {
 	t_stk	*tmp;
 
@@ -23,10 +53,11 @@ void	ft_isdble(t_stk *arg)
 		{
 			tmp = tmp->next;
 			if (arg->num == tmp->num)
-				exit(0);
+				return(1);
 		}
 		arg = arg->next;
 	}
+	return (0);
 }
 
 int		ft_iserr(char *s)
@@ -45,49 +76,46 @@ int		ft_iserr(char *s)
 		return (0);
 }
 
-t_stk	*ft_makenw(char *s)
+void	ft_makenw(int j, char **arr, t_stk **arg)
 {
-	t_stk	*tmp;
-
-	tmp = malloc(sizeof(t_stk));
-	if (tmp != NULL && !ft_iserr(s))
+	if (ft_iserr(arr[j]))
+		ft_abort(arr, arg);
+	if (*arg == NULL)
 	{
-		tmp->num = ft_atoi(s);
-		tmp->next = NULL;
+		*arg = malloc(sizeof(t_stk));
+		if (*arg == NULL)
+			ft_abort(arr, arg);
+		(*arg)->num = ft_atoi(arr[j]);
+		(*arg)->next = NULL;
 	}
 	else
-		exit(0);
-	return (tmp);
+	{
+		(*arg)->next = malloc(sizeof(t_stk));
+		*arg = (*arg)->next;
+		if (*arg == NULL)
+			ft_abort(arr, arg);
+	}
 }
+
 
 void	ft_init(int argc, char **argv, t_stk **arg)
 {
 	int		i;
-	t_stk	*tmp;
 	char	**arr;
 	int		j;
 
 	i = 1;
-	tmp = *arg;
 	while (i < argc)
 	{
 		arr = ft_split(argv[i], ' ');
 		j = 0;
 		while (arr[j])
 		{
-			if (tmp == NULL)
-			{
-				tmp = ft_makenw(arr[j]);
-				*arg = tmp;
-			}
-			else
-			{
-				tmp->next = ft_makenw(arr[j]);
-				tmp = tmp->next;
-			}
+			ft_makenw(j, arr, arg);
 			j++;
 		}
 		i++;
 	}
-	ft_isdble(*arg);
+	if (ft_isdble(*arg))
+		ft_abort(arr, arg);
 }
