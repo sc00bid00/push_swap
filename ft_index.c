10122ca896e114f5ba1137b2500a523e9a1086ab
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-19 13:31:49 +0100

    First commit 	new file:   ft_index.c 	new file:   ft_swapx.c

---
+++ ft_index.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_index.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 12:36:30 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/19 13:30:34 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_swap(t_stk *tmp1, t_stk *tmp2)
+{
+	int	tmpn;
+	int	tmpo;
+	int	tmps;
+
+	tmpn = tmp1->num;
+	tmpo = tmp1->oix;
+	tmps = tmp1->six;
+	tmp1->num = tmp2->num;
+	tmp1->oix = tmp2->oix;
+	tmp1->six = tmp2->six;
+	tmp2->num = tmpn;
+	tmp2->oix = tmpo;
+	tmp2->six = tmps;
+}
+
+void	ft_swapbk(t_stk	**stk)
+{
+	t_stk	*tmp0;
+	t_stk	*tmp;
+
+	tmp0 = *stk;
+	while (tmp0)
+	{
+		if (tmp0->next)
+			tmp = tmp0->next;
+		while (tmp)
+		{
+			if(tmp0->oix > tmp->oix)
+				ft_swap(tmp0, tmp);
+			tmp = tmp->next;
+		}
+		tmp0 = tmp0->next;
+	}
+}
+
+void	ft_srtd(t_stk **stk)
+{
+	int		i;
+	t_stk	*tmp;
+
+	tmp = *stk;
+	i = 0;
+	while (tmp)
+	{
+		tmp->six = i;
+		tmp = tmp->next;
+		i++;
+	}
+}
+
+void	ft_ntrl(t_stk **stk)
+{
+	int		i;
+	t_stk	*tmp;
+
+	tmp = *stk;
+	i = 0;
+	while (tmp)
+	{
+		tmp->oix = i;
+		tmp->six = i;
+		tmp = tmp->next;
+		i++;
+	}
+}
+
+void	ft_index(t_stk **stk)
+{
+	t_stk	*tmp;
+	t_stk	*tmp0;
+
+	ft_ntrl(stk);
+	tmp0 = *stk;
+	while (tmp0)
+	{
+		if (tmp0->next)
+			tmp = tmp0->next;
+		while (tmp)
+		{
+			if (tmp->num < tmp0->num)
+				ft_swap(tmp, tmp0);
+			tmp = tmp->next;
+		}
+		tmp0 = tmp0->next;
+	}
+	ft_srtd(stk);
+	ft_swapbk(stk);
+}
