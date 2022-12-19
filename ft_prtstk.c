61caab520ac2daaf07ae6353d593f20878300216
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-19 14:04:13 +0100

    Modify 	modified:   ft_prtstk.c 	deleted:    ft_swapx.c 	modified:   push_swap.c

--- ft_prtstk.c
+++ ft_prtstk.c
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 08:38:20 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/19 12:58:44 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/19 13:56:14 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_prtstk(t_stk	*arg)
 {
 	while (arg)
 	{
-		ft_printf("%d %d %d\n", arg->oix, arg->six, arg->num);
+		ft_printf("%d\t%d\t%d\n", arg->oix, arg->six, arg->num);
 		arg = arg->next;
 	}
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 17:02:35 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/19 13:11:40 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/19 14:02:49 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 int main(int argc, char **argv)
 {
-	t_stk 	*arg;
+	t_stk 	*sta;
+	t_stk	*stb;
 
 	if (argc > 1)
 	{
-		arg = NULL;
-		ft_init(argc, argv, &arg);
-		ft_index(&arg);
-		ft_prtstk(arg);
-		ft_freearg(&arg);
+		sta = NULL;
+		stb = NULL;
+		ft_init(argc, argv, &sta);
+		ft_index(&sta);
+		ft_prtstk(sta);
+		ft_freearg(&sta);
 	}
 	return (0);
 }
