7d392a62a4e1d739e5116b0bda9b4fcdb940bf2f
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-19 14:42:40 +0100

    First commit 	new file:   pb.c

---
+++ pb.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pb.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 14:37:57 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/19 14:38:25 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pb(t_stk **sta, t_stk **stb)
+{
+	t_stk	*tmp;
+
+	tmp = *sta;
+	*sta = (*sta)->next;
+	if (!stb)
+	{
+		*stb = tmp;
+		(*stb)->next = NULL;
+	}
+	else
+	{
+		tmp->next = *stb;
+		*stb = tmp;
+	}
+	ft_printf("pb\n");
+}
