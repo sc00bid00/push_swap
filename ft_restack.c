8128b645f7d0d3ab7f14f9e1297c6a2ca6004332
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-21 09:59:50 +0100

    Modify 	modified:   ft_restack.c 	new file:   push_swap 	new file:   rslt.txt

--- ft_restack.c
+++ ft_restack.c
@@ -6,69 +6,65 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 18:10:28 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/21 09:06:55 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/21 09:41:08 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_mvtail(t_stk **sta, t_stk **stb, t_var *var)
+void	ft_pushhead(t_stk **sta, t_stk **stb, t_var *var)
 {
 	int	i;
-	int	n;
 
 	var->stbz = ft_stksize(*stb, NULL);
-	n = var->stbz - var->maxp;
-	i = 0;
-	while (i < n)
-	{
-		rrb(stb, 1);
-		i++;
-	}
-	pa(stb, sta);
-	while (i-- >= 1)
-		rb(stb);
-}
-
-void	ft_mvhead(t_stk **sta, t_stk **stb, t_var *var)
-{
-	int	i;
-
 	i = 0;
 	while (i < var->maxp)
 	{
-		rb(stb);
 		i++;
+		rb(stb);
 	}
 	pa(stb, sta);
-	while (i-- >= 1)
+}
+
+void	ft_pushtail(t_stk **sta, t_stk **stb, t_var *var)
+{
+	int	i;
+
+	var->stbz = ft_stksize(*stb, NULL);
+	var->maxp = var->stbz - var->maxp;
+	i = 0;
+	while (i < var->maxp)
+	{
+		i++;
 		rrb(stb, 1);
+	}
+	pa(stb, sta);
 }
 
 void	ft_restack(t_stk **sta, t_stk **stb, t_var *var)
 {
+	t_stk	*tmp;
+	int		i;
+
 	while (ft_stksize(*stb, NULL))
 	{
-		t_stk	*tmp;
-		int	i;
-
 		tmp = *stb;
 		i = 0;
-		var->maxp = i;
 		var->maxn = tmp->six;
+		var->maxp = i;
 		while (tmp->next)
 		{
-			tmp = tmp->next;
 			i++;
+			tmp = tmp->next;
 			if (tmp->six > var->maxn)
 			{
 				var->maxp = i;
 				var->maxn = tmp->six;
 			}
 		}
-		if (i <= ft_stksize(*stb, NULL)/2)
-			ft_mvhead(sta, stb, var);
+		if (var->maxp <= var->stbz / 2)
+			ft_pushhead(sta, stb, var);
 		else
-			ft_mvtail(sta, stb, var);
+			ft_pushtail(sta, stb, var);
 	}
 }
