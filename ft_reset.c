6ed9d0ce9792ecd771059c2b917b84ee5ce10f0f
Luca | lsordo <sordo.luca@gmail.com>
2022-12-27 18:43:19 +0100

    First commit 	new file:   ft_reset.c

---
+++ ft_reset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 18:35:52 by lsordo            #+#    #+#             */
+/*   Updated: 2022/12/27 18:39:01 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_reset(t_stk **sta, t_stk **stb, t_var *var)
+{
+	ft_freearg(sta);
+	ft_freearg(stb);
+	*sta = NULL;
+	*stb = NULL;
+	ft_init(var->argc, var->argv, sta);
+	ft_index(sta, var);
+}
