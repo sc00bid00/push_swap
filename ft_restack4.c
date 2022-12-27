8409b00d606f183a69822ee4137939622c26bbbe
Luca | lsordo <sordo.luca@gmail.com>
2022-12-27 08:22:08 +0100

    Modify
    	modified:   ft_restack4.c
    	modified:   push_swap

--- ft_restack4.c
+++ ft_restack4.c
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 15:47:18 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/26 21:33:16 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/27 08:21:34 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ void	ft_restack(t_stk **sta, t_stk **stb, t_var *var)
 		var->cllr = 0;
 		var->stbz = ft_stksize(*stb, NULL);
 		ft_getmaxv(*stb, var);
-		while ((*stb)->six < var->maxn && (*stb)->six > 0)
+		while ((*stb)->six < var->maxn)
 		{
+			if ((*stb)->six == var->maxn2)
+				pa(sta, stb);
 			if	(var->maxp <= var->stbz / 2)
 				rb(stb);
 			else
 				rrb(stb, 1);
-			if ((*stb)->six == var->maxn2)
-				pa(sta, stb);
 		}
-		if ((*sta) && (*sta)->next && (*sta)->six > (*sta)->next->six)
-			sa(sta, 1);
 		pa(sta, stb);
 		if ((*sta) && (*sta)->next && (*sta)->six > (*sta)->next->six)
 			sa(sta, 1);
