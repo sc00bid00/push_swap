f15ec5a4f3139b6162336089f5a9c688c954cfa4
Luca Sordo <lsordo@2-F-1.42heilbronn.de>
2022-12-28 16:30:37 +0100

    Modify 	modified:   ft_getcmds.c

--- ft_getcmds.c
+++ ft_getcmds.c
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 13:08:41 by lsordo            #+#    #+#             */
-/*   Updated: 2022/12/28 16:12:06 by lsordo           ###   ########.fr       */
+/*   Updated: 2022/12/28 16:28:46 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	ft_getcmds(t_stk **sta, t_stk **stb, t_var *var)
 	{
 		var->cmd = get_next_line(0);
 		if (var->cmd)
+		{
 			ft_act(sta, stb, var);
+			free(var->cmd);
+		}
 		else
 			break ;
 	}
